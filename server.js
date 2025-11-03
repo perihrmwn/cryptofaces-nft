277cfb8a6a95c5de5f308682dbd6cf407fb7b921
Peri Hermawan <76895850+perihrmwn@users.noreply.github.com>
2025-11-03 18:07:21 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
+app.use('/.well-known',
+  express.static(path.join(__dirname, 'public', '.well-known'), { dotfiles: 'allow' })
+);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
